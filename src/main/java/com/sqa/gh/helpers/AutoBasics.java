9165e63a20556f8987f971de9d179ee74668539b
George Hanna <hannagd@yahoo.com>
2017-09-09 12:31:46 -0700

    Add Methods to Read Properties

---
+++ src/main/java/com/sqa/gh/helpers/AutoBasics.java
@@ -0,0 +1,23 @@
+package com.sqa.gh.helpers;
+
+import java.io.*;
+import java.util.*;
+
+import org.testng.log4testng.*;
+
+public class AutoBasics {
+
+	public static String getProp(String propName, String fileLocation, String fileName, Logger logger) {
+		Properties props = new Properties();
+		InputStream input;
+		try {
+			input = new FileInputStream(fileLocation + fileName);
+			props.load(input);
+		} catch (FileNotFoundException e) {
+			logger.warn("Can not load config properties file because it was not found: " + fileName);
+		} catch (IOException e) {
+			logger.warn("Can not load config properties file can not be read: " + fileName);
+		}
+		return props.getProperty(propName);
+	}
+}
